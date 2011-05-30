10bbdac0a105a5d183368c9339b81a769e532220
vios@ylab <mori@.(none)>
2011-05-30 19:26:38 +0900

    change views.py,search_form.html

--- djangobook/chapter7/books/views.py
+++ djangobook/chapter7/books/views.py
@@ -6,10 +6,16 @@ def searchForm(request):
     return render_to_response("search_form.html")
 
 def search(request):
-    if "q" in request.GET and request.GET["q"]:
+    errors = []
+    if "q" in request.GET: 
         q = request.GET["q"]
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response("search_results.html",{"books":books,
-            "query":q})
-    else:
-        return render_to_response("search_form.html",{"error":True})
+        if not q:
+            errors.append("Enter a search term.")
+        elif len(q) > 20:
+            errors.append("Please enter at most 20 characters.")
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response("search_results.html",{"books":books,
+                "query":q})
+
+    return render_to_response("search_form.html",{"errors":errors})
